ea25d466deb0295585534f3952e4f76548d307ad
minigorstudio-tech <minigor.studio@gmail.com>
2025-11-10 18:34:07 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+// import { Toaster } from "@/components/ui/toaster"; // ЗАКОММЕНТИРОВАТЬ
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
@@ -19,7 +19,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
+        {/* <Toaster /> */} {/* ЗАКОММЕНТИРОВАТЬ */}
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
